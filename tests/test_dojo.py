2590238b4f016447e60897279d417d0699a94160
arthur <you@example.com>
2017-04-26 17:49:29 +0300

    unittests

--- tests/test_dojo.py
+++ tests/test_dojo.py
@@ -10,6 +10,13 @@ class CreateRoomTestCase(unittest.TestCase):
 	def test_create_nroom(self):
 		self.dojo.create_room('blue')
 		self.assertEqual(self.dojo.rooms,['blue'],msg='invalid')
+	def test_create_room_successfully(self):
+        	initial_room_count = len(self.dojo.all_rooms)
+        	blue_office = self.dojo.create_room(“Blue”, “office”)
+        	self.assertTrue(blue_office)
+        	new_room_count = len(self.dojo.all_rooms)
+        	self.assertEqual(new_room_count - initial_room_count, 1)
+	
 
 if __name__ == '__main__':
     unittest.main()
